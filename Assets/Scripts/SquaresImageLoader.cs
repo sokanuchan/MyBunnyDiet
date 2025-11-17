9f1c9be1c9eac2fce934052c669458310026c8cc
sokanuchan <clement.huger@epita.fr>
2025-11-17 19:24:37 +0100

    Added Squares image loader script

--- Assets/Scripts/SquaresImageLoader.cs
+++ Assets/Scripts/SquaresImageLoader.cs
@@ -1,13 +1,22 @@
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SquaresImageLoader : MonoBehaviour
 {
-    public Texture2D image;
+    public SpriteRenderer[] squares;
+    public static int bunnyIndex = 1;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // load sprite for each square
+        for (int i = 0; i < squares.Count(); i++)
+        {
+            squares[i].sprite = Resources.Load<Sprite>("Images/Bunnies/Bunny-" + bunnyIndex.ToString() + "/SPlittedPixelArt/sprite_" + i.ToString());
+        }
     }
 
     // Update is called once per frame
