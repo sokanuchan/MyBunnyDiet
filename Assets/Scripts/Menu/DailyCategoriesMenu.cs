c36b185e3f6958b1c34a35f000fc5feb112bb2c9
sokanuchan <clement.huger@epita.fr>
2025-12-18 18:44:00 +0100

    Added date above daily input menu

--- Assets/Scripts/Menu/DailyCategoriesMenu.cs
+++ Assets/Scripts/Menu/DailyCategoriesMenu.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DailyCategoriesMenu : MonoBehaviour
 {
+    public Text date;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // grey the Validate button while calories have not been input
         if (DailyInput.currentDailyInput.calories == 0)
         {
             GameObject.Find("Validate").GetComponent<SpriteRenderer>().color = new Color(0.5f, 0.5f, 0.5f);
         }
+
+        // update date with current input date
+        date.text = DateUtils.ChangeDateFormat(DailyInput.currentDate, DateUtils.dailyInputDateFormat, DateUtils.playerDisplayDateFormat);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Utils/DateUtils.cs
+++ Assets/Scripts/Utils/DateUtils.cs
@@ -5,6 +5,7 @@ using System.Linq;
 public class DateUtils
 {
     public static string dailyInputDateFormat = "yyyy-MM-dd";
+    public static string playerDisplayDateFormat = "dd/MM/yyyy";
 
     public static string GetMonthName(int monthIndex)
     {
@@ -36,5 +37,14 @@ public class DateUtils
 
         DateTime lastInputDate = DateTime.ParseExact(DailyInput.playerInputs.Last().Key, dailyInputDateFormat, null);
         return lastInputDate.AddDays(1);
+    }
+
+    public static string ChangeDateFormat(string date, string currentDateFormat, string outputDateFormat)
+    {
+        // convert date to DateTime object
+        DateTime dateTime = DateTime.ParseExact(date, currentDateFormat, null);
+
+        // convert DateTime object to output format
+        return dateTime.ToString(outputDateFormat);
     }
 }
